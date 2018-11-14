b6490316987940cf42c2b8ee044640c93714f94e
Matthew Mallard <matt@Q-Technologies.com.au>
2018-11-15 10:21:43 +1100

    Fixed merging of baseline selection script config

--- manifests/scripts.pp
+++ manifests/scripts.pp
@@ -5,6 +5,7 @@ class osbaseline::scripts (
   # Get the script paths for Perl scripts
   String $selection_script_path,
   String $selection_config_path,
+  Data $selection_config_default,
   Data $selection_config = lookup('osbaseline::scripts::selection_config', Data, 'deep', {}),
 
 ){
@@ -20,6 +21,8 @@ class osbaseline::scripts (
       content => epp('osbaseline/baseline_selection.pl.epp', { selection_config_path => $selection_config_path })
     }
 
+    $selection_config_merged = $selection_config_default + $selection_config
+
     # Config file for Baseline Selection script
     file { 'baseline_selection_config':
       ensure  => file,
@@ -27,7 +30,7 @@ class osbaseline::scripts (
       owner   => 'root',
       group   => 'root',
       mode    => '0640',
-      content => inline_template('<%= @selection_config.to_yaml %>'),
+      content => inline_template('<%= @selection_config_merged.to_yaml %>'),
     }
   }
 
